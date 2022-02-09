183df71f072fa70f13e3a6a9eb569b5833ffdbad
Philipp Friedrich <freezer05@googlemail.com>
2022-02-09 20:26:23 +0100

    ein paar bunte

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-let malgrund, ball;
+let malgrund, ball, animation;
 const optionen = {
   ball: { size: 24 },
+  running: true,
 };
+
 window.onload = () => {
   malgrund = document.getElementById("malgrund");
-  malgrund.appendChild(createBall());
+  update();
 };
+
+function update() {
+  if (optionen.running) {
+    malgrund.appendChild(createBall());
+    animation = window.requestAnimationFrame(update);
+  }
+}
 
 function createBall(color = "black", size = optionen.ball.size + "px") {
   ball = document.createElement("div");
   ball.style.height = size;
   ball.style.width = size;
-  ball.style.backgroundColor = color;
+  ball.style.backgroundColor = "#" + getColor();
+  ball.style.position = "absolute";
   [randomX, randomY] = getRandomXY();
   ball.style.transform = "translate(" + randomX + "px ," + randomY + "px)";
   ball.style.borderRadius = "50%";
@@ -32,4 +42,9 @@ function getRandomXY() {
 
 function getRandomZahlZwischen(min, max) {
   return Math.random() * (max - min) + min;
+}
+
+function getColor() {
+  const colors = ["047857", "1d4ed8", "db2777"];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
